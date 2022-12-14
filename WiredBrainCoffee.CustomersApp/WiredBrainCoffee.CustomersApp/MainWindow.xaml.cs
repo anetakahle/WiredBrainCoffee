8c94cd357a76ce0989da2b3b6582449abbcb7f38
poolen1 <poolen108@gmail.com>
2022-12-14 14:57:33 -0800

    Refactor with UserControls
    Finished extracting UserControls

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WiredBrainCoffee.CustomersApp
 {
@@ -23,19 +10,6 @@ namespace WiredBrainCoffee.CustomersApp
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
-        {
-            //var column = (int) CustomerListGrid.GetValue(Grid.ColumnProperty);
-
-            //var newColumn = column == 0 ? 2 : 0;
-            //CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
-
-            var column = Grid.GetColumn(CustomerListGrid);
-
-            var newColumn = column == 0 ? 2 : 0;
-            Grid.SetColumn(CustomerListGrid, newColumn);
         }
     }
 }
---
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WiredBrainCoffee.CustomersApp.View
+{
+    /// <summary>
+    /// Interaction logic for CustomersView.xaml
+    /// </summary>
+    public partial class CustomersView : UserControl
+    {
+        public CustomersView()
+        {
+            InitializeComponent();
+        }
+        
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            //var column = (int) CustomerListGrid.GetValue(Grid.ColumnProperty);
+
+            //var newColumn = column == 0 ? 2 : 0;
+            //CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(CustomerListGrid);
+
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(CustomerListGrid, newColumn);
+        }
+    }
+}
