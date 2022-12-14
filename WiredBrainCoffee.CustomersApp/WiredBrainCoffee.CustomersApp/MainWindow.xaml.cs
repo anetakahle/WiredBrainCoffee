1d20d3333a0a74ff8d95cf6cdf5a850539cc66ae
poolen1 <poolen108@gmail.com>
2022-12-13 17:17:06 -0800

    Header UserControl
    Refactored header from MainWindow.xaml into a UserControl.

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -24,6 +24,18 @@ namespace WiredBrainCoffee.CustomersApp
         {
             InitializeComponent();
         }
-        
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            //var column = (int) CustomerListGrid.GetValue(Grid.ColumnProperty);
+
+            //var newColumn = column == 0 ? 2 : 0;
+            //CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(CustomerListGrid);
+
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(CustomerListGrid, newColumn);
+        }
     }
 }
