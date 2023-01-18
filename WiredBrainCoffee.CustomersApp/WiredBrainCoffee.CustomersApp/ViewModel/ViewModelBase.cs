0a20708bc0cc83157dc28ae7ff7da96d1e6d451f
poolen1 <poolen108@gmail.com>
2023-01-17 17:56:40 -0800

    Created MainViewModel class

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/ViewModelBase.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/ViewModelBase.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace WiredBrainCoffee.CustomersApp.ViewModel;
 
@@ -25,4 +26,6 @@ public class ViewModelBase : INotifyPropertyChanged
         OnPropertyChanged(propertyName);
         return true;
     }
+
+    public virtual Task LoadAsync() => Task.CompletedTask;
 }
